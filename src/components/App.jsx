853e9076140c81f4559a62767dcaf75a898a27b4
Neizmailova Natalia <natata.neizmailova@gmail.com>
2023-03-13 10:58:25 +0200

    add BrowserRouter

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { Suspense, lazy } from 'react';
+import React, { lazy } from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 const Home = lazy(() => import('../components/Home/Home'));
@@ -11,20 +11,18 @@ const SharedLayout = lazy(() => import('../others/SharedLayout/SharedLayout'));
 
 const App = () => {
   return (
-  <Suspense>  
     <Routes>
-    <Route path="/" element={<SharedLayout />}>
-      <Route index element={<Home />}></Route>;
-      <Route path="movies" element={<Movies />}></Route>;
-      <Route path="movieDetails" element={<MovieDetails />}>
-        <Route path="cast" element={<Cast />}></Route>;
-        <Route path="reviews" element={<Reviews />}></Route>;
-    </Route>;
+      <Route path="/" element={<SharedLayout />}>
+        <Route index element={<Home />}></Route>;
+        <Route path="movies" element={<Movies />}></Route>;
+        <Route path="movies/:movieId" element={<MovieDetails />}>
+          <Route path="cast" element={<Cast />}></Route>;
+          <Route path="reviews" element={<Reviews />}></Route>;
       </Route>;
+    </Route>;
       
       <Route path="*" element={<NotFound />}></Route>
     </Routes>  
-  </Suspense>
   );
 };
 export default App;
